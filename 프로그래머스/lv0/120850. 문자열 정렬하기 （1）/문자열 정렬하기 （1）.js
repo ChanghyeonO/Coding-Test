e705477082a7eb2cb93b03955ed24247c2a47e9e
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2023-03-23 21:19:48 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,8 +1,7 @@
 function solution(my_string) {
     var answer = [];
     let justNum = my_string.replace(/[a-zA-Z]/g,'');
-    let arr = justNum.split('');
-    arr = arr.sort();
+    let arr = justNum.split('').sort();
 
     for(let i = 0; i < arr.length; i++){
         answer.push(Number(arr[i]))
