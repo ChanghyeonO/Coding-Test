f7fc9b532eaf6dfeb704de649dbb215e3fac44d3
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2023-03-27 23:42:12 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,17 @@
+function solution(numbers, direction) {
+    var answer = [];
+    
+    if(direction == "right"){
+           answer.push(numbers[numbers.length -1])
+        for(let i = 0; i < numbers.length - 1; i++){
+            answer.push(numbers[i]);
+        }
+    }else if(direction == "left"){
+        for(let j = 1; j < numbers.length; j++){
+            answer.push(numbers[j])
+        }
+        answer.push(numbers[0]);
+    }
+    
+    return answer;
+}
