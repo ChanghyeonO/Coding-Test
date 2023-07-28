3e56f8fd1402a7c6b0a1006ae01f0ba21e255257
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2023-07-29 02:20:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,7 @@
 function solution(n) {
     var answer = 0;
     const Check = (Math.sqrt(n))
-   if(Number.isInteger(Check)){
-    answer = (Check + 1) * (Check + 1)
-   }else{
-    answer = -1
-   }
+
+    Number.isInteger(Check) ? (answer = (Check + 1) * (Check + 1)) : (answer = -1);
     return answer;
 }
