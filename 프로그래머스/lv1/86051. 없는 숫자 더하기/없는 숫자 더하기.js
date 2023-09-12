83ef5e6260bd00ea0da8d484784e9b33fb3af869
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2023-09-12 11:10:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     var answer = 0;
-    let arrSort = numbers.sort((a , b) => a - b)
+    let arrSort = numbers
     for(let i = 0; i <= 9; i++){
         if(!arrSort.includes(i)){
            answer += i;
