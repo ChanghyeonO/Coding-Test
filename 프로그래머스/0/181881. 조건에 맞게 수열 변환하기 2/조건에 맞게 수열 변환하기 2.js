5724cd1bdff142877c0fdd1d8720d427e45faee1
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-01-19 19:04:58 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,24 @@
+function solution(arr) {
+    let count = 0;
+
+    while (true) {
+        let changed = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] >= 50 && arr[i] % 2 === 0) {
+                arr[i] = arr[i] / 2;
+                changed = true;
+            } else if (arr[i] < 50 && arr[i] % 2 !== 0) {
+                arr[i] = arr[i] * 2 + 1;
+                changed = true;
+            }
+        }
+
+        if (!changed) {
+            break;
+        }
+
+        count++;
+    }
+
+    return count;
+}
