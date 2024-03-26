90d452e7d2bf852d076a5b78885b6f651d54aeb7
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-03-26 17:32:44 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let answer = num_list
+    const a = num_list[num_list.length - 1];
+    const b = num_list[num_list.length - 2];
+    if(a > b){
+        answer.push(a - b)
+    }else{
+        answer.push(a * 2)
+    }
+   
+    return answer;
+}
