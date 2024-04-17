1ff89ef89f2b7e1d95c7b9c8df701a286094daa7
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-04-17 20:28:33 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    let odd = 0
+    let even = 0
+    for(let i = 0; i < num_list.length; i++){
+        if(i % 2 == 0){
+            even += num_list[i]
+        }else {
+            odd += num_list[i]
+        }
+    }
+    if(odd > even){
+        return odd
+    }else{
+        return even
+    }
+}
