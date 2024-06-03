ef3b52e324f69c517f9e4262e62d964d18707349
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-06-03 19:01:55 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -3,10 +3,12 @@ function solution(emergency) {
     
     // 배열 복사본 생성 이유는 emergency를 직접 sort로 조작하면 원본 배열을 바꿔버려서 값이 다르게 나옴
     let sortArr = [...emergency].sort((a, b) => b - a);
+    // console.log(sortArr)
 
     // for 문을 돌려서 emergency의 길이 만큼 for문을 돌고 sortArr내에 emergency의[i] 째 위치를 찾아서 answer 배열에 push
     for (let i = 0; i < emergency.length; i++) {
-        answer.push(sortArr.indexOf(emergency[i]) + 1)
+       let a = sortArr.indexOf(emergency[i]) + 1
+        answer.push(a)
     }
 
     return answer;
