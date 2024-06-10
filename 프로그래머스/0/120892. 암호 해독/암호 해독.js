1850c282f547595f6b43066309adf7eb1300240d
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-06-10 19:16:41 +0900

    [level 0] Title: 암호 해독, Time: 6.81 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,12 +1,16 @@
 function solution(cipher, code) {
-  let answer = [];
-  let strArr = cipher.split('');
-
+  let answer = []; // 해독된 암호를 저장할 배열 선언
+  let strArr = cipher.split(''); // 문자열을 배열로 변환
+    // console.log(strArr)
+  //1부터 cipher의 길이까지 
   for (let i = 1; i <= cipher.length; i++) {
-    const index = code * i - 1;
-    if (index < strArr.length) {
-      answer.push(strArr[index]);
-    }
+    const index = code * i - 1; // code의 배수 번째 인덱스 계산
+    console.log(index)
+    // if (index < strArr.length) { // 인덱스가 배열 길이보다 작을 때
+      answer.push(strArr[index]); // 해당 인덱스 문자 배열에 추가
+      
+    // }
   }
-  return answer.join('');
+  
+  return answer.join(''); // 배열을 문자열로 변환하여 반환
 }
