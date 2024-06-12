bf85e556b2391c6ed40b5ffe622dd4485f7c8fd5
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-06-13 00:15:03 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 13.56 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,8 +1,23 @@
 function solution(n) {
-    var answer = [];
+    let answer = [];
+    // for문을 사용해 n만큼 배열 먼저 생성 하기
     for (let i = 0; i < n; i++) {
-        answer[i] = new Array(n).fill(0);
-        answer[i][i] = 1;
+        let arr = [];
+        // console.log(arr)
+        
+        // 이중 for문 사용해서 n의 갯수만큼 생성된 배열에 n개만큼 0을 집어넣음
+        for (let j = 0; j < n; j++) {
+            arr.push(0); 
+        }
+        
+        // splice에 들어가는 매개변수 첫번째는 n번째 위치 지목, 
+        // 가운데는 n번째 위치부터 n개를 선택,
+        // 마지막은 대체할 요소를 넣는다
+        arr.splice(i, 1, 1); 
+        console.log(arr)
+        
+        // 이렇게 생성된 배열을 answer에다가 push
+        answer.push(arr);
     }
     return answer;
 }
