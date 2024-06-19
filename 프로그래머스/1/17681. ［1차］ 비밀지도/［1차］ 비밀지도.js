8bb8cd25a94c6b93f8694ab2d03b6849d1130743
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-06-19 17:06:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,29 @@
+function solution(n, arr1, arr2) {
+ let answer = [];
+ let a = arr1.map(idx => {
+        let str = idx.toString(2); 
+        return ("0000000000" + str).slice(-n);
+    });
+    
+
+    let b = arr2.map(idx => {
+        let str = idx.toString(2)
+        return ("0000000000" + str).slice(-n);
+    }); 
+    
+    // console.log(a, b)
+     for (let i = 0; i < n; i++) {
+        let c = '';
+        for (let j = 0; j < n; j++) {
+            if (a[i][j] === '1' || b[i][j] === '1') {
+                c += '#';
+            } else {
+                c += ' ';
+            }
+        }
+        answer.push(c);
+    }
+    
+    
+    return answer;
+}
