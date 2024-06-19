55c80a5cbe14d84529c8bb2e1932f8bd982fa1a1
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-06-19 17:55:27 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.37 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,23 +1,29 @@
 function solution(n, arr1, arr2) {
-    
+ let answer = [];
  let a = arr1.map(idx => {
-        let binStr = idx.toString(2);  // 2진수로 변환
-        // 앞에 필요한 만큼의 0을 추가한 후, slice로 뒤에서 n개의 문자만 취함
-        return ("0000000000" + binStr).slice(-n);
+        let str = idx.toString(2); 
+        return ("00000000" + str).slice(-n);
     });
     
-    // arr2도 동일한 방식으로 처리
-    let b = arr2.map(idx => {
-        let binStr = idx.toString(2);  // 2진수로 변환
-        // 앞에 필요한 만큼의 0을 추가한 후, slice로 뒤에서 n개의 문자만 취함
-        return ("0000000000" + binStr).slice(-n);
-    });
 
-    console.log(a);  // arr1의 2진수 배열
-    console.log(b);  // arr2의 2진수 배열
+ let b = arr2.map(idx => {
+        let str = idx.toString(2)
+        return ("00000000" + str).slice(-n);
+    }); 
     
     console.log(a, b)
+     for (let i = 0; i < n; i++) {
+        let c = '';
+        for (let j = 0; j < n; j++) {
+            if (a[i][j] === '1' || b[i][j] === '1') {
+                c += '#';
+            } else {
+                c += ' ';
+            }
+        }
+        answer.push(c);
+    }
     
-    var answer = [];
+    
     return answer;
 }
