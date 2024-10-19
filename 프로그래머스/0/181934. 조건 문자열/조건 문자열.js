eba180f35ab0712feb7dbc4a75bba8062cff3eeb
ChanghyeonO <114368832+ChanghyeonO@users.noreply.github.com>
2024-10-19 17:25:04 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,19 +1,14 @@
 function solution(ineq, eq, n, m) {
-    var answer = 0;
-    if(ineq === ">"){
-        if(eq === "="){
-            n >= m == true ? answer = 1 : 0
-        }else {
-            n > m == true ? answer = 1 : 0
-        }
-        
-    }else if(ineq === "<"){
-        if(eq === "="){
-            n <= m == true ? answer = 1 : 0
-        }else {
-            n < m == true ? answer = 1 : 0
-        }
-        
+    if(ineq === ">" && eq === "="){
+        return n >= m == true ? 1 : 0;
     }
-    return answer;
+    if(ineq === "<" && eq === "="){
+        return n <= m == true ? 1 : 0;
+    } 
+    if(ineq === ">" && eq === "!"){
+        return n > m == true ? 1 : 0;
+    }
+    if(ineq === "<" && eq === "!"){
+        return n < m == true ? 1 : 0;
+    } 
 }
